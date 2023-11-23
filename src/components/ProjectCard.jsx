fb0fc46ef5ffac21b26cdc48be4ea2619edb9898
freelancing with programming <iosattbsoft2021@gmail.com>
2023-11-23 12:12:39 +0500

    mmm

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,5 +1,3 @@
-import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
-import { TbWorldWww } from 'react-icons/tb';
 
 const ProjectCard = ({ url, img, github, title, text }) => {
   return (
@@ -14,11 +12,9 @@ const ProjectCard = ({ url, img, github, title, text }) => {
         <p className='mt-4 text-slate-700 leading-loose'>{text}</p>
         <div className='mt-4 flex gap-x-4'>
           <a href={url}>
-            <TbWorldWww className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
-          </a>
+           </a>
           <a href={github}>
-            <FaGithubSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
-          </a>
+            </a>
         </div>
       </div>
     </article>
--- src/data.jsx
+++ src/data.jsx
@@ -1,5 +1,4 @@
 import { nanoid } from 'nanoid';
-
 export const links = [
   { id: nanoid(), href: '#home', text: 'home' },
   { id: nanoid(), href: '#skills', text: 'skills' },
